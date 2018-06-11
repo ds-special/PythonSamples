31d26904fc6faa457acc3a27b6ba89f3065d83e1
Paul Sykes <paul.sykes@dealogic.com>
2018-06-11 12:02:57 +0100

    first edits

---
+++ PythonSamples/PythonSamples.py
@@ -0,0 +1,13 @@
+print ("I will now count my chickens:")
+print ("Hens", 25 + 30 / 6)
+print ("Roosters", 100 - 25* 3 % 4)
+
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
